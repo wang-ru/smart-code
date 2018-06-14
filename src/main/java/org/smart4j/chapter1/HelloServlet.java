70000e9562c839d86d956b3f740cdc08b5d7ca27
wangru <wangru1@baozun.com>
2018-06-14 16:56:04 +0800

    代码修改2

--- src/main/java/org/smart4j/chapter1/HelloServlet.java
+++ src/main/java/org/smart4j/chapter1/HelloServlet.java
@@ -18,8 +18,7 @@ public class HelloServlet extends HttpServlet
     @Override
     protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
         SimpleDateFormat dateFormat = new SimpleDateFormat("yyyy-MM-dd");
-        String currentTime = dateFormat.format(new Date());
-        req.setAttribute("currentTime",currentTime);
+        System.out.println("hello");
         req.getRequestDispatcher("/WEB-INF/jsp/hello.jsp").forward(req,resp);
     }
 }
